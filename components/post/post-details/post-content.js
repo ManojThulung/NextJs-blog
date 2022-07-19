3922103786ab9a8133668f6cd14d78f720740567
Manoj Rai <manojthulung03@gmail.com>
2022-07-19 16:53:16 +0545

    implemented react-syntax-highlighter

--- components/post/post-details/post-content.js
+++ components/post/post-details/post-content.js
@@ -2,6 +2,8 @@ import ReactMarkdown from "react-markdown";
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 function PostContent(props) {
   const { post } = props;
@@ -31,6 +33,19 @@ function PostContent(props) {
       }
 
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      console.log(code);
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
 
